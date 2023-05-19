2d3f2c90bde3c7147896af1c0ef4a6b5ebead8a3
productdevbook <hi@productdevbook.com>
2023-05-19 14:24:25 +0300

    feat: release

---
+++ scripts/add-tag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# get the version number from package.json 
+version=$(node -p -e "require('./package.json').version")
+# git tag it
+git tag $version
+
+# push it
+git push origin $version
